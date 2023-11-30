7e9b084d4454014390a7547c311171cd0cc6d7fa
aditisah <sditi.sah1@gmail.com>
2023-11-30 13:30:19 +0530

    signup and otp verification template added

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
+import { OtpVerificationComponent } from './auth/otp-verification/otp-verification.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo: '', pathMatch: 'full'},
+  {path: 'signup', component: SignupComponent},
+  {path: 'otp-verification', component: OtpVerificationComponent}
+  // {path: 'country-map', component: CountryMapChartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { OtpVerificationComponent } from './auth/otp-verification/otp-verification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    OtpVerificationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
